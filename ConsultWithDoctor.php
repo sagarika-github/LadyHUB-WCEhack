f12aadcd736ded3a3bad672be5874ba786d46022
Sagarika <sagarikamondal197@gmail.com>
2022-06-22 20:56:32 +0530

    minor bugs fixed

---
+++ ConsultWithDoctor.php
@@ -0,0 +1,66 @@
+<html>
+
+<head>
+    <!-- <link rel="stylesheet" href="./CSS/consult.css"> -->
+    <link rel="stylesheet" href="./CSS/home-style.css">
+    <style>
+        html {
+            background: url("./assets/img8.webp");
+        }
+    </style>
+</head>
+<header id="header">
+    <div id="brand">
+        <img id="header-img" src="assets/icon1.png" href="home.php" />
+    </div>
+    <nav id="nav-bar">
+        <a class="nav-link" href="home.php">Home</a>
+        <a class="nav-link" href="FindGroup.php">Events</a>
+        <a class="nav-link" href="ConsultWithDoctor.php">Consult</a>
+        <a class="nav-link" href="Blog.php">Blog</a>
+        <a class="nav-link" href="profile.php">Profile</a>
+    </nav>
+</header>
+
+<body style="margin-top: 200px;">
+    <div id="home-content" style="border: 5px solid black;color:#2d2d2d;">
+        <h1 style="font-size:50px; color:darkblue">Connect with Doctors and Counselor</h1>
+    </div>
+    <div id="content-home" style="display: flex; flex-direction:column; align-items:center;justify-content:center">
+        <div id='doc-results'>
+            <div class='product-card' style="width: 40%;background:linear-gradient(black,white,0.5)">
+                <img src='assets/doc.jpg' class='product-img'></img>
+                <div class='product-content'>
+                    <h1 class='product-name'>Dr Shimali Patle</h1>
+                    <h3 style="color:#ff3256;">Gynaecologist(M.S.) MBBS</h3>
+                    <!-- <a href="./video_call/index.php"><button class="doc-button" style="padding: 0.6rem;margin:2rem 0 2rem 0;border-radius:5rem;background-color:#ff3256;cursor:pointer;border:5px solid #0dd;color:aliceblue;font-size:1rem;font-weight:600">Connect</Button></a> -->
+                    <a href="https://demo.videowhisper.com/html5-videochat-php/"><button class="doc-button" style="padding: 0.6rem;margin:2rem 0 2rem 0;border-radius:5rem;background-color:#ff3256;cursor:pointer;border:5px solid #0dd;color:aliceblue;font-size:1rem;font-weight:600">Connect</Button></a>
+                </div></a>
+            </div>
+        </div>
+        <div id='doc-results'>
+            <div class='product-card' style="width:40%;background:linear-gradient(black,white,0.5)">
+                <img src='assets/doc.jpg' class='product-img'></img>
+                <div class='product-content'>
+                    <h1 class='product-name'>Sakshi Bhandarkar</h1>
+                    <h3 style="color:#ff3256;">Counselor and Therapist</h3>
+                    <!-- <a href="./video_call/index.php"><button class="doc-button" style="padding: 0.6rem;margin:2rem 0 2rem 0;border-radius:5rem;background-color:#ff3256;cursor:pointer;border:5px solid #0dd;color:aliceblue;font-size:1rem;font-weight:600">Connect</Button></a> -->
+                    <a href="https://demo.videowhisper.com/html5-videochat-php/"><button class="doc-button" style="padding: 0.6rem;margin:2rem 0 2rem 0;border-radius:5rem;background-color:#ff3256;cursor:pointer;border:5px solid #0dd;color:aliceblue;font-size:1rem;font-weight:600">Connect</Button></a>
+                </div></a>
+            </div>
+        </div>
+        <div id='doc-results'>
+            <div class='product-card' style="width:40%;background:linear-gradient(black,white,0.5)">
+                <img src='assets/doc.jpg' class='product-img'></img>
+                <div class='product-content'>
+                    <h1 class='product-name'>Dr Anjali Patle</h1>
+                    <h3 style="color:#ff3256;">Pyschology(M.S.) MBBS</h3>
+                    <!-- <a href="./video_call/index.php"><button class="doc-button" style="padding: 0.6rem;margin:2rem 0 2rem 0;border-radius:5rem;background-color:#ff3256;cursor:pointer;border:5px solid #0dd;color:aliceblue;font-size:1rem;font-weight:600">Connect</Button></a> -->
+                    <a href="https://demo.videowhisper.com/html5-videochat-php/"><button class="doc-button" style="padding: 0.6rem;margin:2rem 0 2rem 0;border-radius:5rem;background-color:#ff3256;cursor:pointer;border:5px solid #0dd;color:aliceblue;font-size:1rem;font-weight:600">Connect</Button></a>
+                </div></a>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
---
+++ DietPlans.php
@@ -0,0 +1,64 @@
+<html>
+
+<head>
+    <link rel="stylesheet" href="./CSS/home-style.css">
+    <!-- <link rel="stylesheet" href="./CSS/consult.css"> -->
+    <style>
+        html {
+            background: url(./assets/img5.jpg);
+            /* background-size: cover; */
+        }
+    </style>
+</head>
+<header id="header">
+    <div id="brand">
+        <img id="header-img" src="assets/icon1.png" href="home.php" />
+    </div>
+    <nav id="nav-bar">
+        <a class="nav-link" href="home.php">Home</a>
+        <a class="nav-link" href="FindGroup.php">Events</a>
+        <a class="nav-link" href="ConsultWithDoctor.php">Consult</a>
+        <a class="nav-link" href="Blog.php">Blog</a>
+        <a class="nav-link" href="profile.php">Profile</a>
+    </nav>
+</header>
+
+<body style="margin-top: 150px;">
+    <div id="home-content">
+        <h1 style="font-size:50px;">Diet Plans</h1>
+    </div>
+    <div id="content-home" style="display:flex;flex-direction:column;width:40rem;margin-left:17rem">
+        <div id='doc-results'>
+            <div class='product-card' style="background: #0dd;">
+                <img src='assets/diet-plan.jpg' class='product-img'></img>
+                <div class='product-content' style="text-align: center;">
+                    <h1 class='product-name'>Ketogenic Diet</h1>
+                    <h3 style="color:white;margin-top:0.6rem; margin-bottom:0.5rem">Low carb diet for weight loss</h3>
+                    <button style="background-color: #4285F4;cursor:pointer" onclick="document.location='https://calendar.google.com/calendar/u/0?cid=MnUxbzdxaWE3cDRzNXZmOWJzb3VmNHFpZWtAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ'" class="doc-button">Add to my calender</Button>
+                </div></a>
+            </div>
+        </div>
+        <div id='doc-results'>
+            <div class='product-card' style="background: #0dd;">
+                <img src='assets/diet-plan.jpg' class='product-img'></img>
+                <div class='product-content' style="text-align: center;">
+                    <h1 class='product-name'>Indian Diet plan for weight loss</h1>
+                    <h3 style="color:white;margin-top:0.6rem; margin-bottom:0.5rem">Low carb diet for weight loss</h3>
+                    <button style="background-color: #4285F4;cursor:pointer; " onclick="document.location='https://calendar.google.com/calendar/u/0?cid=aGxndHJsYTdoZGliZGc5ZmJvZmwwbTRiNDRAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ'" class="doc-button">Add to my calender</Button>
+                </div></a>
+            </div>
+        </div>
+        <div id='doc-results'>
+            <div class='product-card' style="background: #0dd;">
+                <img src='assets/diet-plan.jpg' class='product-img'></img>
+                <div class='product-content' style="text-align: center;">
+                    <h1 class='product-name'>High protein meal plan</h1>
+                    <h3 style="color:white;margin-top:0.6rem; margin-bottom:0.5rem">Low carb diet for weight loss</h3>
+                    <button style="background-color: #4285F4;cursor:pointer" onclick="document.location='https://calendar.google.com/calendar/u/0?cid=aGxndHJsYTdoZGliZGc5ZmJvZmwwbTRiNDRAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ'" class="doc-button">Add to my calender</Button>
+                </div></a>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
