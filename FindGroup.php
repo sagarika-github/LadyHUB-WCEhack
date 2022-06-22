abe41bd69a74e6d413a1ca549df4d28a0a622084
Anwesha Chakraborty <acanne.297@gmail.com>
2022-06-22 21:18:02 +0530

    Changes stashed

---
+++ FindGroup.php
@@ -0,0 +1,171 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+
+<head>
+  <link rel="stylesheet" href="CSS/home-style.css">
+  <meta charset="utf-8">
+  <title>Community Engagement Activities</title>
+  <style>
+    html {
+      background: linear-gradient(to right, #fbc2eb 0%, #a6c1ee 100%);
+    }
+
+    .container {
+
+      width: 400px;
+      padding: 2rem;
+      height: 300px;
+      background: linear-gradient(to right, #FEBF01 0%, #A9F5F2 100%);
+
+    }
+
+
+    .container:hover {
+      /* display: none; */
+      color: #ff3256;
+      background: #1f1f1f;
+    }
+
+    .block {
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+
+
+    }
+
+    left,
+    .right {
+      height: 50%;
+      width: 50%;
+      position: fixed;
+      overflow-x: hidden;
+      padding-top: 20px;
+    }
+
+    .left {
+      left: 0;
+      float: left;
+      width: 50%;
+    }
+
+    .bbt {
+      padding: 1rem;
+      background: #ff3256;
+      font-size: 1.2rem;
+      font-weight: 600;
+      border-radius: 5rem;
+      width: 50%;
+    }
+
+    .bbt:hover {
+      color: #fff;
+      background-color: #0dd;
+
+    }
+
+    .right {
+      right: 0;
+      float: right;
+    }
+  </style>
+</head>
+<header id="header">
+  <div id="brand">
+    <img id="header-img" src="assets/icon1.png" href="home.php" />
+  </div>
+  <nav id="nav-bar">
+    <a class="nav-link" href="home.php">Home</a>
+    <a class="nav-link" href="FindGroup.php">Events</a>
+    <a class="nav-link" href="ConsultWithDoctor.php">Consult</a>
+    <a class="nav-link" href="Blog.php">Blog</a>
+    <a class="nav-link" href="profile.php">Profile</a>
+  </nav>
+</header>
+
+<body style="margin-top: 200px;">
+
+
+  <h1 style="margin-left:35%;margin-top:5%;color:#061818;font-family: fantasy;"><b>Community Engagement Activities</b></h1><br>
+
+  <!-- <div class="block"> -->
+  <div class="left">
+    <div class='container' style='text-align:center;margin: 30px auto; border-radius: 15px; box-shadow: 0px 2px 2px #ff3256;border: 2px solid;overflow:none'>
+      <h2 style='font-family:Arial, Helvetica, sans-serif;'><b>Art of Living Session</b></h2>
+      <p><b>Location</b></p>
+      <p style="color:#0101DF;">Bhilai Center, Bhilai, Chhattisgarh, India</p>
+      <p><b> Organizer</b></p>
+      <p style="color:#0101DF">Sudarshan Kriya Practice Groups in Bhilai</p>
+      <p><b> Description</b></p>
+      <p style="color:#0101DF">Join The Online Meditation &
+        Breath Workshop. Learn Yoga and Meditation</p>
+
+      <div class="" style=" margin:3rem;font-size:2rem;">
+        <button class="bbt" style="">Sign Up</button>
+      </div>
+
+    </div>
+
+  </div>
+  <div class="left">
+    <div class='container' style='text-align:center;margin: 30px auto; border-radius: 15px; box-shadow: 0px 2px 2px #ff3256;border: 2px solid;overflow:none'>
+      <h2 style='font-family:Arial, Helvetica, sans-serif;'><b>Art of Living Session</b></h2>
+      <p><b>Location</b></p>
+      <p style="color:#0101DF;">Bhilai Center, Bhilai, Chhattisgarh, India</p>
+      <p><b> Organizer</b></p>
+      <p style="color:#0101DF">Sudarshan Kriya Practice Groups in Bhilai</p>
+      <p><b> Description</b></p>
+      <p style="color:#0101DF">Join The Online Meditation &
+        Breath Workshop. Learn Yoga and Meditation</p>
+      <div class="" style=" margin:3rem;font-size:2rem;">
+        <button class="bbt" style="">Sign Up</button>
+      </div>
+
+
+    </div>
+
+  </div>
+  <div class="left">
+    <div class='container' style='text-align:center;margin: 30px auto; border-radius: 15px; box-shadow: 0px 2px 2px #ff3256;border: 2px solid;overflow:none'>
+      <h2 style='font-family:Arial, Helvetica, sans-serif;'><b>Art of Living Session</b></h2>
+      <p><b>Location</b></p>
+      <p style="color:#0101DF;">Bhilai Center, Bhilai, Chhattisgarh, India</p>
+      <p><b> Organizer</b></p>
+      <p style="color:#0101DF">Sudarshan Kriya Practice Groups in Bhilai</p>
+      <p><b> Description</b></p>
+      <p style="color:#0101DF">Join The Online Meditation &
+        Breath Workshop. Learn Yoga and Meditation</p>
+      <div class="" style=" margin:3rem;font-size:2rem;">
+        <button class="bbt" style="">Sign Up</button>
+      </div>
+
+    </div>
+
+  </div>
+
+  <div class="left">
+    <div class='container' style='text-align:center;margin: 30px auto; border-radius: 15px; box-shadow: 0px 2px 2px #ff3256;border: 2px solid;overflow:none'>
+      <h2 style='font-family:Arial, Helvetica, sans-serif;'><b>Training on Mental Health Aid</b></h2>
+      <p><b>Location</b></p>
+      <p style="color:#0101DF;">The Ruby, Bombay Chamber, 4th Floor, NW 29 Senapati Bapat Marg, Dadar (West), Mumbai 400028</p>
+      <p><b> Organizer</b></p>
+      <p style="color:#0101DF">Bombay Chamber of Commerce and Industry</p>
+      <p><b> Description</b></p>
+      <p style="color:#0101DF">Build awareness or enhance general awareness of Signs & symptoms of mental illness .</p>
+      <div class="" style=" margin:3rem;font-size:2rem;">
+        <button class="bbt" style="">Sign Up</button>
+      </div>
+
+    </div>
+    <!-- </div> -->
+
+
+    <br>
+
+
+  </div>
+
+
+</body>
+
+</html>
---
+++ home.php
@@ -0,0 +1,58 @@
+<html>
+
+<head>
+  <link rel="stylesheet" href="CSS/home-style.css">
+  <link href="https://fonts.googleapis.com/css2?family=Dancing+Script:wght@600&display=swap" rel="stylesheet">
+
+  <meta name="viewport" content="width=device-width,initial-scale=1.0">
+</head>
+
+<body style="background-image: url('https://media.istockphoto.com/photos/galaxy-outer-space-starry-sky-purple-red-abstract-star-pattern-picture-id1347283792?b=1&k=20&m=1347283792&s=170667a&w=0&h=xCJGDFW4tX_REGXIKIaMTaRqAl4TPoKxFSXCGGGBJyo=');">
+
+
+  <header id="header">
+    <div id="brand">
+      <img id="header-img" src="assets/icon1.png" href="home.php" />
+    </div>
+    <nav id="nav-link">
+      <a class="nav-link" href="home.php">Home</a>
+      <a class="nav-link" href="FindGroup.php">Events</a>
+      <a class="nav-link" href="ConsultWithDoctor.php">Consult</a>
+      <a class="nav-link" href="Blog.php">Blog</a>
+      <a class="nav-link" href="profile.php">Profile</a>
+    </nav>
+  </header>
+
+
+  <div id="health-tracker-card" style="background-image: url('https://thumbs.dreamstime.com/b/abstract-dark-green-gradient-background-waves-folds-d-illustration-volumetric-strict-design-business-card-invitation-208169468.jpg');">
+    <a href="MentalAndPhysical.php" style="text-decoration:none;">
+      <h1 style="color:white; font-size:72px;">Check your<br> health status</h1>
+    </a>
+    <img src="assets/health-track-img.png" id="health-track-img">
+  </div>
+  <div id="period-tracker-card" style="background-color:#fb5454 ;">
+    <a href="Tracker.php" style="text-decoration:none;">
+      <h1 style="color:white; font-size:48px;">Period Tracker</h1>
+    </a>
+  </div>
+  <div>
+    <div style="display:flex; justify-content:center;">
+      <div id="sub-card" style="background-image: url('https://thumbs.dreamstime.com/b/dark-blue-wavy-lines-abstract-background-business-card-green-gradient-template-cover-banner-design-your-company-143231885.jpg');">
+        <a href="DietPlans.php" style="text-decoration:none;">
+          <h1 style="color:white; margin:60px 15px 0px 15px;">Nutrition and Diet Plans</h1>
+        </a>
+      </div>
+      <div id="sub-card" style="background-image: url('https://thumbs.dreamstime.com/b/dark-blue-wavy-lines-abstract-background-business-card-green-gradient-template-cover-banner-design-your-company-143231885.jpg');">
+        <a href="forum.php" style="text-decoration:none;">
+          <h1 style="color:white; margin:60px 15px 0px 15px;">Community Forum</h1>
+        </a>
+      </div>
+      <div id="sub-card" style="background-image: url('https://thumbs.dreamstime.com/b/dark-blue-wavy-lines-abstract-background-business-card-green-gradient-template-cover-banner-design-your-company-143231885.jpg');">
+        <a href="a.php" style="text-decoration:none;">
+          <h1 style="color:white; margin:60px 15px 0px 15px;">Wellness Products</h1>
+        </a>
+      </div>
+    </div>
+  </div>
+</body>
+<html>
